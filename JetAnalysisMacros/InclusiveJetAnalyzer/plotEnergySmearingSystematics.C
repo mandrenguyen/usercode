34fa4029bab8135776658c238c21d525dc614500
kimy <>
2010-12-06 23:23:12 +0000

    5Bins

--- JetAnalysisMacros/InclusiveJetAnalyzer/plotEnergySmearingSystematics.C
+++ JetAnalysisMacros/InclusiveJetAnalyzer/plotEnergySmearingSystematics.C
@@ -54,32 +54,51 @@ TH1D *getErrorBand(TH1* h);
 
 void plotEnergySmearingSystematics(){
 
-  TCanvas *c1 = new TCanvas("c1","",1250,530);
+   TCanvas *c1 = new TCanvas("c1","",1650,430);
 
-  makeMultiPanelCanvas(c1,3,1,0.0,0.0,0.2,0.15,0.02);
+   makeMultiPanelCanvas(c1,5,1,0.0,0.0,0.2,0.15,0.02);
+
 
   c1->cd(1);
-  plotRatio(2,"data-smeared.root","data.root","data.root",false,false,false);
-  drawText("30~100%",0.76,0.24);
+  plotRatio(4,"data-smeared.root","data.root","data.root",false,false,false);
+  drawText("50~100%",0.72,0.24);
   drawPatch(0.976,0.0972,1.1,0.141);
+  gPad->SetLeftMargin(0.25);
+  gPad->SetBottomMargin(0.18);
 
   c1->cd(2);
-  plotRatio(1,"data-smeared.root","data.root","data.root",false,true,false);
-  drawText("10~30%",0.75,0.24);
+  plotRatio(3,"data-smeared.root","data.root","data.root",false,false,false);
+  drawText("30~50%",0.7,0.24);
   drawPatch(-0.00007,0.0972,0.0518,0.141);
   drawPatch(0.976,0.0972,1.1,0.141);
+  gPad->SetBottomMargin(0.18);
 
   c1->cd(3);
-  plotRatio(0,"data-smeared.root","data.root","data.root",false,false,true);
-  drawText("0~10%",0.75,0.24);
+  plotRatio(2,"data-smeared.root","data.root","data.root",false,true,false);
+  drawText("20~30%",0.7,0.24);
   drawPatch(-0.00007,0.0972,0.0518,0.141);
+  drawPatch(0.976,0.0972,1.1,0.141);
+  gPad->SetBottomMargin(0.18);
+
+  c1->cd(4);
+  plotRatio(1,"data-smeared.root","data.root","data.root",false,false,false);
+  drawText("10~20%",0.7,0.24);
+  drawPatch(-0.00007,0.0972,0.0518,0.141);
+  drawPatch(0.976,0.0972,1.1,0.141);
+  gPad->SetBottomMargin(0.18);
+
+  c1->cd(5);
+  plotRatio(0,"data-smeared.root","data.root","data.root",false,false,true);
+  drawText("0~10%",0.73,0.24);
+  drawPatch(-0.00007,0.0972,0.0518,0.141);
+  gPad->SetBottomMargin(0.18);
 
   TLatex *cms = new TLatex(0.086,4.5,"CMS Preliminary");
   cms->SetTextFont(63);
   cms->SetTextSize(18);
   cms->Draw();                                                                                                                                        
 
-  TLatex *lumi = new TLatex(0.15,4.1,"#intL dt = 3.4 #mub^{-1}");
+  TLatex *lumi = new TLatex(0.15,4.1,"#intL dt = 7 #mub^{-1}");
   lumi->SetTextFont(63);
   lumi->SetTextSize(15);
   lumi->Draw(); 
@@ -101,15 +120,23 @@ void plotRatio(int cbin,
   TString cut="et1>120&& et1<2000 && et2>50 && dphi>2.5&&(et1-et2)/(et1+et2)<10 ";
   TString cstring = "";
   if(cbin==0) {
-    cstring = "0-10%";
-    cut+=" && bin>=0 && bin<4";
+     cstring = "0-10%";
+     cut+=" && bin>=0 && bin<4";
   } else if (cbin==1) {
-    cstring = "10-30%";
-    cut+=" && bin>=4 && bin<12";
-  } else {
-    cstring = "30-100%";
-    cut+=" && bin>=12 && bin<40";
+     cstring = "10-20%";
+     cut+=" && bin>=4 && bin<8";
+  } else if (cbin==2) {
+     cstring = "20-30%";
+     cut+=" && bin>=8 && bin<12";
+  } else if (cbin==3) {
+     cstring = "30-50%";
+     cut+=" && bin>=12 && bin<20";
   }
+  else {
+     cstring = "50-100%";
+     cut+=" && bin>=20";
+  }
+
 
   // open the data file
   TFile *inf = new TFile(infname.Data());
