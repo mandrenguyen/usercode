444a997a269697d569ddf820c507a041232bc960
yjlee <>
2010-12-07 12:25:58 +0000

    Updated systematic check related stuff

--- JetAnalysisMacros/InclusiveJetAnalyzer/analyzeJetTrees.C
+++ JetAnalysisMacros/InclusiveJetAnalyzer/analyzeJetTrees.C
@@ -5,7 +5,9 @@
 #include "TTree.h"
 #include "TROOT.h"
 #include "TNtuple.h"
+#include "TRandom.h"
 #include "TF1.h"
+#include "TTimeStamp.h"
 
 // Hacks to get the jet energy scale uncertainty
 #include "CondFormats/JetMETObjects/src/Utilities.cc"
@@ -18,7 +20,7 @@
 
 
 // Note that centrality flag is somewhat deprecated, just use central = 0 to make the tree and make cuts in the plotting macros
-int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, int useAK5PF=0){
+int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, int useAK5PF=0, int correctAwaySideJet = 0, int smearEnergy = 0, int simulateIneff = 0){
   
   string infile;
   string outfile;
@@ -50,6 +52,10 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
   //central =1, 0-10
   //central =2, 10-30
   // central =3, 30-90
+
+  // Set Random Seed
+  TTimeStamp myTime;
+  gRandom->SetSeed(myTime.GetNanoSec());
   
   // Jet energy correction
   JetCorrectionUncertainty *jecUnc = new JetCorrectionUncertainty("CondFormats/JetMETObjects/data/Spring10_Uncertainty_AK5Calo.txt");
@@ -58,6 +64,10 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
   TF1 *fRes = new TF1("fRes","TMath::Gaus(x,[0],[1])");
   fRes->SetParameters(1,0.1);
   fRes->SetRange(0,2);
+
+  TF1 *fSmear = new TF1("fRes","TMath::Gaus(x,[0],[1])");
+  fSmear->SetParameters(0,7);
+  fSmear->SetRange(-35,35);
 
   cout<< "opening file: "<<infile<<endl;
   
@@ -102,8 +112,8 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
    Int_t           lumi;
    Float_t         b;
    Float_t         hf;
-   Int_t         nref;
-   Int_t         ngen;
+   Int_t           nref;
+   Int_t           ngen;
    Int_t           bin;
    Float_t         jtpt[99];
    Float_t         jteta[99];
@@ -151,7 +161,7 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
    // create output file 
    TFile *fout=new TFile(outfile.c_str(),"RECREATE");
    // add a small ntuple for light analysis
-   TNtuple *nt = new TNtuple("nt","","et1:eta1:phi1:unc1:et2:eta2:phi2:unc2:bin:dphi:weight:fRes");
+   TNtuple *nt = new TNtuple("nt","","et1:eta1:phi1:unc1:et2:eta2:phi2:unc2:et3:eta3:phi3:bin:dphi:dphi2:weight:fRes:run");
 
 
    float pi= acos(-1.);
@@ -173,7 +183,7 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
    TH2F *hLeadingResolutionVsPt = new TH2F("hLeadingResolutionVsPt","hLeadingResolutionVsPt",100,-5.,5.,100,0,500);
    TH2F *hSubLeadingResolutionVsPt = new TH2F("hSubLeadingResolutionVsPt","hSubLeadingResolutionVsPt",100,-5.,5.,100,0,500);
 
-   TFile *fcent_Data = new TFile("CentDist_Data_v10.root");
+   TFile *fcent_Data = new TFile("CentDist_Data_v13.root");
    TH1F *hCent_Data = (TH1F*)fcent_Data->Get("h");
    float cent_integral_Data = 1.;
    if(central==1)cent_integral_Data=hCent_Data->Integral(1,4);
@@ -186,9 +196,9 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
    Long64_t nbytes = 0;
    int count =0;
    for (Long64_t i=0; i<nentries;i++) {
+      if (i% 10000 == 0) cout <<run<<" "<<i<<" "<<count<<endl;
       nbytes += t->GetEntry(i);
       if (nref==0) continue;
-
 
       //Marguerite's HCAL filterering identified these events, but too late for a repass so remove by hand
       if(data_pythia_mix==0){
@@ -236,17 +246,18 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
 	if(run== 152791 && evt == 246343 && lumi ==  39  ) continue; 
       }
 
-
-
-      
       int max_jet_index = -1;
       int sub_jet_index = -1;
+      int third_jet_index = -1;
       float max_jet_pt = 0.;
       float sub_jet_pt = 0.;
+      float third_jet_pt = 0.;
       float max_jet_phi = 0.;
       float sub_jet_phi = 0.;
+      float third_jet_phi = 0.;
       float max_jet_eta = 0.;
       float sub_jet_eta = 0.;
+      float third_jet_eta = 0.;
 
       //if(bin<10) continue;
       if(central==1){
@@ -272,26 +283,59 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
       
       max_jet_index = 0;
       max_jet_pt = jtpt[0];
+      if (smearEnergy) max_jet_pt+=fSmear->GetRandom();
       max_jet_phi = jtphi[0];
       max_jet_eta = jteta[0];
       
       for(int ijet=1;ijet<nref;ijet++){
-	
+        double candPt = jtpt[ijet];
+	if (correctAwaySideJet) {
+	   //double cor=(1-0.221219/candPt-427.546/candPt/candPt);
+	   //double cor=0.652738+0.00408465*candPt-(9.95783e-6)*candPt*candPt;
+	   //double cor= (-13.02)/(candPt+0.2071)-87.64/candPt/candPt+1.151;
+	   //double cor= (-3.013)/(candPt+89.01)-75.44/candPt/candPt+1.071;
+	   double cor= 1.3895 -0.0161403*candPt + 0.000186386 *candPt * candPt -(6.4896e-7)*candPt*candPt*candPt;
+	   if (candPt>140) cor = 1;
+	   cout <<cor<<endl;
+	   candPt/=cor;
+	} 
+        if (smearEnergy) candPt+=fSmear->GetRandom();
+        if (simulateIneff) {
+           if (candPt<60&&gRandom->Rndm()<0.1) {
+              candPt=0;
+           }
+        }	
         if(fabs(jteta[ijet]) < 2 // this is the value that has been changed from 2 -> 3
-           && jtpt[ijet]> sub_jet_pt)
+           && candPt > sub_jet_pt)
           {
+	    third_jet_index = sub_jet_index;
+	    third_jet_pt = sub_jet_pt;
+	    third_jet_phi = sub_jet_phi;
+	    third_jet_eta = sub_jet_eta;
+
             sub_jet_index = ijet;
-            sub_jet_pt = jtpt[ijet];
+            sub_jet_pt = candPt;
             sub_jet_phi = jtphi[ijet];
             sub_jet_eta = jteta[ijet];
+          } else if (fabs(jteta[ijet]) < 2 // this is the value that has been changed from 2 -> 3
+           && candPt > third_jet_pt)
+          {
+            third_jet_index = ijet;
+            third_jet_pt = candPt;
+            third_jet_phi = jtphi[ijet];
+            third_jet_eta = jteta[ijet];
           }
-	
       }
 
       float dphi = max_jet_phi - sub_jet_phi;
       if(dphi<0) dphi+=2*pi;
       if(dphi>2*pi) dphi-=2*pi;
       if(dphi>pi) dphi = 2*pi - dphi;
+
+      float dphi2 = max_jet_phi - third_jet_phi;
+      if(dphi2<0) dphi2+=2*pi;
+      if(dphi2>2*pi) dphi2-=2*pi;
+      if(dphi2>pi) dphi2 = 2*pi - dphi2;
 
       hLeadingJetPt->Fill(max_jet_pt,weight);
       hPt1_Pt2_Dphi->Fill(max_jet_pt,sub_jet_pt,dphi,weight);
@@ -311,7 +355,26 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
       double sub_unc = jecUnc->getUncertainty(true);
 
       // Fill mini-ntuple
-      nt->Fill(max_jet_pt,max_jet_eta,max_jet_phi,max_unc,sub_jet_pt,sub_jet_eta,sub_jet_phi,sub_unc,bin,dphi,weight,fRes->GetRandom());
+      Float_t var[100];
+      var[0]=max_jet_pt;
+      var[1]=max_jet_eta;
+      var[2]=max_jet_phi;
+      var[3]=max_unc;
+      var[4]=sub_jet_pt;
+      var[5]=sub_jet_eta;
+      var[6]=sub_jet_phi;
+      var[7]=sub_unc;
+      var[8]=third_jet_pt;
+      var[9]=third_jet_eta;
+      var[10]=third_jet_phi;
+      var[11]=bin;
+      
+      var[12]=dphi;
+      var[13]=dphi2;
+      var[14]=weight;
+      var[15]=fRes->GetRandom();
+      var[16]=run;
+      nt->Fill(var);
       count++;
       /*  // This would need to be updated to use gen info
       if(isMC){
@@ -347,6 +410,7 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
 
    // Write to output file.
    fout->cd();
+   nt->SetAlias("aj","(et1-et2)/(et1+et2)");
    nt->Write();
 
    hLeadingJetPt->Write();
