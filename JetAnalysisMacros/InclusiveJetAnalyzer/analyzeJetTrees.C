17d15cd5b36f1e2f24748031523a9a943f841f32
mnguyen <>
2010-12-07 08:46:11 +0000

    Added HCAL cleaning

--- JetAnalysisMacros/InclusiveJetAnalyzer/analyzeJetTrees.C
+++ JetAnalysisMacros/InclusiveJetAnalyzer/analyzeJetTrees.C
@@ -26,18 +26,19 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
   int isMC = 0;
 
   if(data_pythia_mix==0){
-    infile = "/castor/cern.ch/user/m/mnguyen//HIDATA/JetTrees/merged_jetTree_Data_v10.root";
+    infile = "/castor/cern.ch/user/m/mnguyen//HIDATA/JetTrees/merged_jetTree_Data_v13.root";
     if(useAK5PF) outfile = "data_AK5PF.root";
     else outfile  = "data.root";
   }
   if(data_pythia_mix==1){
-    infile = "/castor/cern.ch/user/m/mnguyen//HIDATA/JetTrees/merged_jetTree_Pyquen_v2.root";
+    infile = "/castor/cern.ch/user/m/mnguyen//HIDATA/JetTrees/merged_jetTree_Pyquen_pt80_v4.root";
     if(useAK5PF) outfile = "pythia_AK5PF.root";
     else outfile  = "pythia.root";
     isMC=1;
   }
   if(data_pythia_mix==2){
     infile = "/afs/cern.ch/user/e/edwenger/public/dijets/dijetTreeDM_pt80.root";
+      //infile = "/castor/cern.ch/user/m/mnguyen//HIDATA/JetTrees/merged_jetTree_Pyquen_DataEmbedded_pt80_v1.root";
     if(useAK5PF) outfile = "mix_AK5PF.root";
     else outfile  = "mix.root";
     useWeight = 1;
@@ -64,9 +65,14 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
   TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject(infile.c_str());
 
   char *dirname=NULL;
-  if(useAK5PF)dirname = "ak5PFJetAnalyzer";
-  else dirname = "inclusiveJetAnalyzer";
-
+  if(data_pythia_mix==0 || data_pythia_mix==2){
+    if(useAK5PF)dirname = "ak5PFJetAnalyzer";
+    else dirname = "inclusiveJetAnalyzer";
+  }
+  else{
+    if(useAK5PF)dirname = "ak5PFJetAnalyzer_ppMC";
+    else dirname = "inclusiveJetAnalyzer_ppMC";
+  }
   cout<<" looking in directory: "<<dirname<<endl;
 
   if (!f) {
@@ -93,22 +99,25 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
 //Declaration of leaves types
    Int_t           run;
    Int_t           evt;
+   Int_t           lumi;
    Float_t         b;
    Float_t         hf;
    Int_t         nref;
+   Int_t         ngen;
    Int_t           bin;
    Float_t         jtpt[99];
    Float_t         jteta[99];
    Float_t         jty[99];
    Float_t         jtphi[99];
-   Float_t         refpt[99];
-   Float_t         refeta[99];
-   Float_t         refy[99];
-   Float_t         refphi[99];
-   Float_t         refdrjt[99];
+   Float_t         genpt[99];
+   Float_t         geneta[99];
+   Float_t         geny[99];
+   Float_t         genphi[99];
+   Float_t         gendrjt[99];
 
    // Set branch addresses.
    t->SetBranchAddress("run",&run);
+   t->SetBranchAddress("lumi",&lumi);
    t->SetBranchAddress("evt",&evt);
    t->SetBranchAddress("b",&b);
    t->SetBranchAddress("hf",&hf);
@@ -126,11 +135,12 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
    t->SetBranchAddress("jtphi",jtphi);
    
    if(isMC){
-     t->SetBranchAddress("refpt",refpt);
-     t->SetBranchAddress("refeta",refeta);
-     t->SetBranchAddress("refy",refy);
-     t->SetBranchAddress("refphi",refphi);
-     t->SetBranchAddress("refdrjt",refdrjt);
+     t->SetBranchAddress("ngen",&ngen);
+     t->SetBranchAddress("genpt",genpt);
+     t->SetBranchAddress("geneta",geneta);
+     t->SetBranchAddress("geny",geny);
+     t->SetBranchAddress("genphi",genphi);
+     t->SetBranchAddress("gendrjt",gendrjt);
    }
 
 //     This is the loop skeleton
@@ -178,6 +188,56 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
    for (Long64_t i=0; i<nentries;i++) {
       nbytes += t->GetEntry(i);
       if (nref==0) continue;
+
+
+      //Marguerite's HCAL filterering identified these events, but too late for a repass so remove by hand
+      if(data_pythia_mix==0){
+        if(run== 151211 && evt == 555881 && lumi ==  103 ) continue;
+	if(run== 151238 && evt == 581194 && lumi ==  105 ) continue;
+	if(run== 151238 && evt == 1299690 && lumi ==  242) continue;
+	if(run== 151352 && evt == 55195 && lumi ==  10	 ) continue;
+	if(run== 151878 && evt == 594808 && lumi ==  135 ) continue;
+	if(run== 152047 && evt == 666877 && lumi ==  114 ) continue;
+	if(run== 152112 && evt == 2207843 && lumi ==  426) continue;
+	if(run== 152112 && evt == 3151220 && lumi ==  608) continue;
+	if(run== 152349 && evt == 939393 && lumi ==  220 ) continue;
+	if(run== 152350 && evt == 595632 && lumi ==  111 ) continue;
+	if(run== 152350 && evt == 2482917 && lumi ==  472) continue;
+	if(run== 152350 && evt == 2686548 && lumi ==  512) continue;
+	if(run== 152474 && evt == 2085185 && lumi ==  403) continue;
+	if(run== 152477 && evt == 1890056 && lumi ==  392) continue;
+	if(run== 152485 && evt == 55917 && lumi ==  12	 ) continue;
+	if(run== 152561 && evt == 3406888 && lumi ==  606) continue;
+	if(run== 152561 && evt == 3758331 && lumi ==  670) continue;
+	if(run== 152561 && evt == 4478132 && lumi ==  803) continue;
+	if(run== 152561 && evt == 4797830 && lumi ==  863) continue;
+	if(run== 152561 && evt == 5176016 && lumi ==  936) continue;
+	if(run== 152592 && evt == 22234 && lumi ==  4	 ) continue;
+	if(run== 152592 && evt == 402212 && lumi ==  66	 ) continue;
+	if(run== 152594 && evt == 587793 && lumi ==  110 ) continue;
+	if(run== 152601 && evt == 1417393 && lumi ==  267) continue;
+	if(run== 152602 && evt == 686565 && lumi ==  111 ) continue;
+	if(run== 152602 && evt == 3940942 && lumi ==  684) continue;
+	if(run== 152624 && evt == 995626 && lumi ==  170 ) continue;
+	if(run== 152624 && evt == 1063452 && lumi ==  182) continue;
+	if(run== 152624 && evt == 1250655 && lumi ==  215) continue;
+	if(run== 152624 && evt == 1846646 && lumi ==  322) continue;
+	if(run== 152625 && evt == 1634959 && lumi ==  282) continue;
+	if(run== 152625 && evt == 3162245 && lumi ==  563) continue;
+	if(run== 152641 && evt == 173478 && lumi ==  31	 ) continue;
+	if(run== 152642 && evt == 359181 && lumi ==  57	 ) continue;
+	if(run== 152642 && evt == 1764595 && lumi ==  286) continue;
+	if(run== 152642 && evt == 2686223 && lumi ==  446) continue;
+	if(run== 152721 && evt == 1304320 && lumi ==  249) continue;
+	if(run== 152721 && evt == 1983505 && lumi ==  358) continue;
+	if(run== 152722 && evt == 2963949 && lumi ==  485) continue;
+	if(run== 152741 && evt == 558533 && lumi ==  91	 ) continue;
+	if(run== 152751 && evt == 3432123 && lumi ==  582) continue;
+	if(run== 152791 && evt == 246343 && lumi ==  39  ) continue; 
+      }
+
+
+
       
       int max_jet_index = -1;
       int sub_jet_index = -1;
@@ -253,7 +313,7 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
       // Fill mini-ntuple
       nt->Fill(max_jet_pt,max_jet_eta,max_jet_phi,max_unc,sub_jet_pt,sub_jet_eta,sub_jet_phi,sub_unc,bin,dphi,weight,fRes->GetRandom());
       count++;
-
+      /*  // This would need to be updated to use gen info
       if(isMC){
 
 	float max_ref_pt = refpt[max_jet_index];
@@ -282,7 +342,7 @@ int analyzeJetTrees(int data_pythia_mix=2, int central = 0, int useRawPt = 0, in
 	if(sub_jet_index>-1 && sub_ref_pt>0)hSubLeadingResolutionVsPt->Fill((sub_jet_pt-sub_ref_pt)/sub_ref_pt,sub_ref_pt,weight);
 
       }
-
+      */
    }
 
    // Write to output file.
