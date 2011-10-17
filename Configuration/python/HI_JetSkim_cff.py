c25e5531153147c34a64c53448bd4052246b0376
yjlee <>
2011-10-17 15:35:03 +0000

    Remove HCAL reflagger

--- Configuration/python/HI_JetSkim_cff.py
+++ Configuration/python/HI_JetSkim_cff.py
@@ -43,10 +43,10 @@ from CmsHi.PhotonAnalysis.hiEcalRecHitSpikeFilter_cfi import *
 hiEcalRecHitSpikeFilter.minEt = 20.0
 
 #HCAL cleaning
-from JetMETAnalysis.HcalReflagging.hbherechitreflaggerJETMET_cfi import *
+#from JetMETAnalysis.HcalReflagging.hbherechitreflaggerJETMET_cfi import *
 
-hbheReflagNewTimeEnv = hbherechitreflaggerJETMET.clone()
-hbheReflagNewTimeEnv.timingshapedcutsParameters.hbheTimingFlagBit=cms.untracked.int32(8)
+#hbheReflagNewTimeEnv = hbherechitreflaggerJETMET.clone()
+#hbheReflagNewTimeEnv.timingshapedcutsParameters.hbheTimingFlagBit=cms.untracked.int32(8)
 
 # HCAL Timing
 hcalTimingFilter = cms.EDFilter("HcalTimingFilter",
@@ -68,7 +68,7 @@ jetSkimSequence = cms.Sequence(hltJetHI
                                * jetEtFilter
                                #* dijetEtFilter
                                * hiEcalRecHitSpikeFilter
-                               * hbheReflagNewTimeEnv
+                               #* hbheReflagNewTimeEnv
                                * hcalTimingFilter
                                * HBHENoiseFilter
                                )
