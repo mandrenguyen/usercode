e030235a4bc72d94d4ff62a863b4bc96585af251
mnguyen <>
2011-12-06 16:47:45 +0000

    fixed bug in analyzer name

--- ElectronAnalyzer/python/electronAnalyzer_cff.py
+++ ElectronAnalyzer/python/electronAnalyzer_cff.py
@@ -1,17 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
-electronAnalyzer = cms.EDAnalyzer("electronAnalyzer",
-                                      eleTag = cms.InputTag("ecalDrivenGsfElectrons"),
-                                      eventInfoTag = cms.InputTag("generator"),
-                                      isMC = cms.untracked.bool(True), 
-                                      useCentrality = cms.untracked.bool(True),
-                                      L1gtReadout = cms.InputTag("gtDigis"),
-                                      hltTrgResults = cms.untracked.string("TriggerResults::HLT"),
-                                      #put some triggers you care about
-                                      hltTrgNames  = cms.untracked.vstring('HLT_HIMinBiasHfOrBSC_Core',
-                                                                           'HLT_HIJet35U',
-                                                                           'HLT_HIJet35U_Core',
-                                                                           'HLT_HIJet50U_Core')
-                                      )
+electronAnalyzer = cms.EDAnalyzer("ElectronAnalyzer",
+                                  eleTag = cms.InputTag("ecalDrivenGsfElectrons"),
+                                  mcTag = cms.InputTag("generator"),
+                                  isMC = cms.untracked.bool(True),
+                                  useVtx = cms.untracked.bool(False),
+                                  useCentrality = cms.untracked.bool(False),
+                                  L1gtReadout = cms.InputTag("gtDigis"),
+                                  hltTrgResults = cms.untracked.string("TriggerResults::HLT"),
+                                  #put some triggers you care about
+                                  hltTrgNames  = cms.untracked.vstring('HLT_HIMinBiasHfOrBSC_Core',
+                                                                       'HLT_HIJet35U',
+                                                                       'HLT_HIJet35U_Core',
+                                                                       'HLT_HIJet50U_Core')
+                                  )
 
 
