5acc67d1dc2e461ea28abf98eb4474ae802c84e7
yilmaz <>
2011-12-08 15:25:40 +0000

    random numbers

---
+++ JetAnalysisMacros/InclusiveJetAnalyzer/makeRandom.C
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include "TCanvas.h"
+#include "TRandom.h"
+#include "TFile.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TNtuple.h"
+
+
+
+void makeRandom(int N = 1000000){
+
+  TFile* outf = new TFile("random.root","recreate");
+  TNtuple* nt = new TNtuple("ntRandom","","unc1:unc2");
+
+  TRandom* gen = new TRandom();
+
+  for(int i = 0; i < N; ++i){
+    double r1 = gen->Gaus();
+    double r2 = gen->Gaus();
+
+    nt->Fill(r1,r2);
+  }
+
+  nt->Draw("r1");
+  nt->Draw("r2","","same");
+
+}
+
